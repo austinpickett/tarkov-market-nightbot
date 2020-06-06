6c28ae7ff72be344e6c1b31ab7f04fb63231be8c
Austin Pickett <pickett.austin@gmail.com>
2020-06-06 19:55:35 -0400

    multiple words

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php
 $query = $_GET['q'];
 $query = str_replace(' ', '+', $query);
+
 if (empty($query)) {
   echo 'To use market bot type !m <short_name>. No spaces allowed.';
   die;
 }
 
-$url = "https://tarkov-market.com/api/v1/item?q=$query";
+$url = 'https://tarkov-market.com/api/v1/item?q='. $query;
 
-//  Initiate curl
 $ch = curl_init();
-// Will return the response, if false it print the response
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'x-api-key:b0Awg5B02bx7IOjN',
+  'x-api-key:'.getenv('API_KEY'),
 ));
 
 curl_setopt($ch, CURLOPT_URL,$url);
 $result = json_decode(curl_exec($ch), true)[0];
 curl_close($ch);
 
-print_r($result);
-
+if (empty($result['name'])) {
+  echo 'Error in name';
+  die;
+}
 
 echo $result['name']. ": "  .number_format((int)$result['price']);
